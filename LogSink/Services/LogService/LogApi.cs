f4f1a30dafe2697062daa38d6b9b559d8917bfc5
Trey Adams <tadams@redventures>
2018-12-13 14:28:36 -0500

    Added basic unit tests

--- LogSink/Services/LogService/LogApi.cs
+++ LogSink/Services/LogService/LogApi.cs
@@ -1,35 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace LogSink.Services.LogService
+﻿namespace LogSink.Services.LogService
 {
     public class LogApi : ILogApi
     {
-        public ActionResult<LogResponse> Debug(string log)
+        public LogResponse Debug(string log)
         {
             return new LogResponse();
         }
 
-        public ActionResult<LogResponse> Error(string log)
+        public LogResponse Error(string log)
         {
             return new LogResponse();
         }
 
-        public ActionResult<LogResponse> Fatal(string log)
+        public LogResponse Fatal(string log)
         {
             return new LogResponse();
         }
 
-        public ActionResult<LogResponse> Info(string log)
+        public LogResponse Info(string log)
         {
             return new LogResponse();
         }
 
-        public ActionResult<LogResponse> Trace(string log)
+        public LogResponse Trace(string log)
         {
             return new LogResponse();
         }
 
-        public ActionResult<LogResponse> Warn(string log)
+        public LogResponse Warn(string log)
         {
             return new LogResponse();
         }
